a0b4633df14f6c497ef3bb928fdadbb806d878c7
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-05-29 19:42:29 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for (char c : s) 
+        {
+            if (isalnum(c)) 
+            {
+                str += tolower(c);
+            }
+        }
+        int n = str.size();
+
+        int left = 0, right = n - 1;
+        while(left < right)
+        {
+            if(str[left] != str[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
