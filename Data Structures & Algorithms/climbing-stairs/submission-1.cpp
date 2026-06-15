3cc49212a2c6e0dc2f9d4f4c2ccdeb461424bfd2
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-06-15 23:26:04 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int helper(vector<int>&dp, int n)
+    {
+        if(n <= 1)
+        {
+            return 1; 
+        }
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+
+        return dp[n] = helper(dp, n - 1) + helper(dp, n - 2);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        dp[0] = dp[1] = 1;
+        return helper(dp, n);
+    }
+};
