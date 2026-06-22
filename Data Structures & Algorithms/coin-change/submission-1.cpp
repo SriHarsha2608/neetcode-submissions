c69687a2029b3e82b6f87eed0cbf23dce7a46e3a
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-06-22 15:58:59 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int helper(vector<int>& coins, int amount, vector<int> &dp)
+    {
+        if(amount == 0)
+        {
+            return 0;
+        }
+
+        if(dp[amount] != -1)
+        {
+            return dp[amount];
+        }
+
+        int ans = 1e9;
+
+        for(int i = 0; i < coins.size(); i++)
+        {   
+            if(amount >= coins[i])
+            {            
+                ans = min(ans, 1 + helper(coins, amount - coins[i], dp));
+            }   
+        }
+
+        return dp[amount] = ans;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+
+        vector<int> dp(amount + 1, -1);
+
+        int ans = helper(coins, amount, dp);
+        if(ans == 1e9)
+        {
+            return -1;
+        }
+        else
+        {
+            return ans;
+        }
+    }
+};
