f559939f993db0473c4b8ec181f9e095f3cd45f4
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-06-24 15:13:39 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double pow(double x, int n)
+    { 
+        if(n == 0)
+        {
+            return 1;
+        }
+
+        if(n % 2 == 0)
+        {
+            return pow(x * x, n / 2);
+        }
+        else
+        {
+            return x * pow(x * x, (n - 1) / 2);
+        }
+    }
+
+    double myPow(double x, int n) {
+        if(x == 0)
+        {
+            return 0;
+        }
+        
+        if(n == 0)
+        {
+            return 1;
+        }
+
+        double ans = pow(x, abs(n));
+        return n >= 0 ? ans : 1 / ans;
+
+    }
+};
