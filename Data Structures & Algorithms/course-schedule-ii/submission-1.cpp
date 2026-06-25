39fb5133f5a5b5d4911f1a2ec4677b263bcc2c92
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-06-26 01:24:13 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        for(int i = 0; i < prerequisites.size(); i++)
+        {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+
+        vector<int> topo;
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it: adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it] == 0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+
+        return topo.size() == numCourses ? topo: vector<int>{};
+    }
+};
