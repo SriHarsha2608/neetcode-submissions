8ac6db135b671508c87fc2a2e035d07e0a99c838
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-07-01 20:41:52 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char ch: s)
+        {
+            if(ch == '{' || ch == '(' || ch == '[')
+            {
+                st.push(ch);
+            }
+            else if(ch == ')')
+            {
+                if(st.empty()) return false;
+
+                if(st.top() == '(') st.pop();
+                else return false;
+            }
+            else if(ch == '}')
+            {
+                if(st.empty()) return false;
+
+                if(st.top() == '{') st.pop();
+                else return false;
+            }
+            else if(ch == ']')
+            {
+                if(st.empty()) return false;
+
+                if(st.top() == '[') st.pop();
+                else return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
