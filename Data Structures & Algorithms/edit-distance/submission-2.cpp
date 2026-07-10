edf8c6d88848a24e9d7dd42c6a57c291b6f40ddc
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-07-11 00:53:07 +0530

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.cpp
@@ -0,0 +1,59 @@
+class Solution {
+private:
+    int f(string word1, string word2, int i, int j, vector<vector<int>>& dp)
+    {
+        if(i == 0)
+        {
+            return j + 1;
+        }
+        if(j == 0)
+        {
+            return i + 1;
+        }
+
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        if(word1[i - 1] == word2[j - 1])
+        {
+            return dp[i][j] = f(word1, word2, i - 1, j - 1, dp);
+        }
+        else
+        {
+            return dp[i][j] = 1 + min(f(word1, word2, i, j - 1, dp), min(f(word1, word2, i - 1, j, dp), f(word1, word2, i - 1, j - 1, dp)));
+        }
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int j = 0; j <= m; j++)
+        {
+            dp[0][j] = j;
+        }
+
+        for(int i = 0; i <= n; i++)
+        {
+            dp[i][0] = i;
+        }
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1]));
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
