37475d672bc775f4174f946a9ea63f70d91d9dc5
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-07-11 00:57:54 +0530

    Add: edit-distance - submission-5

---
+++ Data Structures & Algorithms/edit-distance/submission-5.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+
+        if(n < m)
+        {
+            return minDistance(word2, word1);
+        }
+        
+        vector<int> prev(m + 1, 0);
+
+        for(int j = 0; j <= m; j++)
+        {
+            prev[j] = j;
+        }
+
+        for(int i = 1; i <= n; i++)
+        {
+            vector<int> cur(m + 1, 0);
+            cur[0] = i;
+
+            for(int j = 1; j <= m; j++)
+            {
+                if(word1[i - 1] == word2[j - 1])
+                {
+                    cur[j] = prev[j - 1];
+                }
+                else
+                {
+                    cur[j] = 1 + min(cur[j - 1], min(prev[j], prev[j - 1]));
+                }
+            }
+
+            prev = cur;
+        }
+        
+        return prev[m];
+    }
+};
