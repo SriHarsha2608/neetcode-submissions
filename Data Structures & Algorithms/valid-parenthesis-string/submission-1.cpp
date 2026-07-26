06a1fabcf363ea7452e1021057c5a2b90b7e5deb
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-07-26 16:17:41 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mini = 0, maxi = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') {
+                mini += 1;
+                maxi += 1;
+            } else if(s[i] == ')') {
+                mini -= 1;
+                mini = max(0, mini);
+                maxi -= 1;
+                if(maxi < 0) {
+                    return false;
+                }
+            } else {
+                mini -= 1;
+                mini = max(0, mini);
+                maxi += 1;
+            }
+        }
+
+        return mini == 0;
+    }
+};
