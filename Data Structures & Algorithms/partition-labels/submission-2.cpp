59a67e7efbb502230becb848ca3b10b7dc342e8e
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-07-26 16:11:40 +0530

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        map<char, int> lastOccurence;
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(lastOccurence.find(s[i]) == lastOccurence.end()) {
+                lastOccurence[s[i]] = i;
+            }
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            int last = lastOccurence[s[i]];
+
+            int j = i + 1;
+            while(j < last) {
+                last = max(last, lastOccurence[s[j]]);;
+                j++;
+            }
+            ans.push_back(last - i + 1);
+            i = last;
+        }
+
+        return ans;
+    }
+};
