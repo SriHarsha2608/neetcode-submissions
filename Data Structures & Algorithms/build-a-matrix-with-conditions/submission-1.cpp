09108f8cc7749f7725fc398a49597a4fe427d628
SriHarsha2608 <saisriharsha.g@students.iiit.ac.in>
2026-07-28 17:20:24 +0530

    Add: build-a-matrix-with-conditions - submission-1

---
+++ Data Structures & Algorithms/build-a-matrix-with-conditions/submission-1.cpp
@@ -0,0 +1,71 @@
+class Solution {
+private:
+    unordered_set<int> visit, path;
+    vector<int> order;
+
+    vector<int> topoSort(int k, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        for(int i = 1; i <= k; i++) {
+            adj[i] = {};
+        }
+
+        for(auto& edge: edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        visit.clear();
+        path.clear();
+        order.clear();
+        for(int i = 1; i <= k; i++) {
+            if(visit.find(i) == visit.end()) {
+                if(!dfs(i, adj)) {
+                    return {};
+                }
+            }
+        }
+
+        reverse(order.begin(), order.end());
+        return order;
+    }
+
+    bool dfs(int src, unordered_map<int, vector<int>>& adj) {
+        if(path.find(src) != path.end()) return false;
+        if(visit.find(src) != visit.end()) return true;
+
+        visit.insert(src);
+        path.insert(src);
+        for(int it: adj[src]) {
+            if(!dfs(it, adj)) {
+                return false;
+            }
+        }
+
+        path.erase(src);
+        order.push_back(src);
+        return true;
+    }
+
+public:
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        vector<int> rowOrder = topoSort(k, rowConditions);
+        if(rowOrder.empty()) return {};
+        vector<int> colOrder = topoSort(k, colConditions);
+        if(colOrder.empty()) return {};
+
+        unordered_map<int, int> valToRow, valToCol;
+        for(int i = 0; i < rowOrder.size(); i++) {
+            valToRow[rowOrder[i]] = i;
+        }
+        for(int i = 0; i < colOrder.size(); i++) {
+            valToCol[colOrder[i]] = i;
+        }
+
+        vector<vector<int>> res(k, vector<int>(k, 0));
+        for(int num = 1; num <= k; num++) {
+            int r = valToRow[num], c = valToCol[num];
+            res[r][c] = num;
+        }
+
+        return res;
+    }
+};
